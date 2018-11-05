81219b44457d9864712923669424ebdac8de4ead
Nick <aliasatrick@gmail.com>
2018-11-05 03:56:37 +0300

    beta 2

--- js/index.js
+++ js/index.js
@@ -12,3 +12,29 @@ $button.on('click', function(e){
       $button.addClass('open');
     }
 });
+
+
+$(function () {
+
+    $('#up').click(function () {
+        $('html, body').animate({scrollTop: 0}, 500);
+        return false;
+    })
+
+})
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+        $('header').addClass("sticky");
+    }
+    else {
+        $('header').removeClass("sticky");
+    }
+});
+
+$(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+        $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
